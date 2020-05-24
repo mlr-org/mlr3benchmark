5002b2efe2213c017d8ff30ccb6e36a1efa17b53
Raphael <raphael.sonabend.15@ucl.ac.uk>
2020-05-24 09:57:49 +0100

    update suggests

--- R/plot_shaps.R
+++ R/plot_shaps.R
@@ -1,6 +1,3 @@
-library(mlr3)
-library(ggplot2)
-
 get_shaps <- function(model, task, data = NULL) {
   if (is.null(data)) {
     data = as.matrix(task$data(cols = task$feature_names))
@@ -45,25 +42,35 @@ plot_shap_feature <- function(shaps, feature,
   shaps = shaps[complete.cases(shaps), ]
   type = match.arg(type)
   if (type == "b") {
-    ggplot(shaps %>% subset(Var == feature), aes(x = Truth, y = Shap)) +
-      geom_point(color = points_color) + geom_smooth(se = se, fill = smooth_fill, color = smooth_color,
+    ggplot2::ggplot(subset(shaps, Var == feature),
+                    ggplot2::aes(x = Truth, y = Shap)) +
+      ggplot2::geom_point(color = points_color) +
+      ggplot2::geom_smooth(se = se, fill = smooth_fill, color = smooth_color,
                                                      level = smooth_level) +
-      labs(x = xlab, y = "SHAP Value") + theme_minimal() +
-      theme(panel.grid = element_blank(),
-            axis.line.x = element_line(), axis.line.y = element_line())
+      ggplot2::labs(x = xlab, y = "SHAP Value") +
+      ggplot2::theme_minimal() +
+      ggplot2::theme(
+        panel.grid = ggplot2::element_blank(),
+        axis.line.x = ggplot2::element_line(),
+        axis.line.y = ggplot2::element_line())
   } else if (type == "p") {
-    ggplot(shaps %>% subset(Var == feature), aes(x = Truth, y = Shap)) +
-      geom_point(color = points_color) +
-      labs(x = xlab, y = "SHAP Value") + theme_minimal() +
-      theme(panel.grid = element_blank(),
-            axis.line.x = element_line(), axis.line.y = element_line())
+    ggplot2::ggplot(subset(shaps, Var == feature),
+                    ggplot2::aes(x = Truth, y = Shap)) +
+      ggplot2::geom_point(color = points_color) +
+      ggplot2::labs(x = xlab, y = "SHAP Value") + theme_minimal() +
+      ggplot2::theme(panel.grid = ggplot2::element_blank(),
+                     axis.line.x = ggplot2::element_line(),
+                     axis.line.y = ggplot2::element_line())
   } else {
-    ggplot(shaps %>% subset(Var == feature), aes(x = Truth, y = Shap)) +
-      geom_smooth(se = se, lwd = smooth_lwd, fill = smooth_fill, color = smooth_color,
+    ggplot2::ggplot(subset(shaps, Var == feature),
+                    ggplot2::aes(x = Truth, y = Shap)) +
+      ggplot2::geom_smooth(se = se, lwd = smooth_lwd, fill = smooth_fill, color = smooth_color,
                   level = smooth_level) +
-      labs(x = xlab, y = "SHAP Value") + theme_minimal() +
-      theme(panel.grid = element_blank(),
-            axis.line.x = element_line(), axis.line.y = element_line())
+      ggplot2::labs(x = xlab, y = "SHAP Value") +
+      ggplot2::theme_minimal() +
+      ggplot2::theme(panel.grid = ggplot2::element_blank(),
+                     axis.line.x = ggplot2::element_line(),
+                     axis.line.y = ggplot2::element_line())
   }
 
 }
@@ -71,27 +78,49 @@ plot_shap_feature <- function(shaps, feature,
 plot_shap_summary <- function(shaps, low = "blue", high = "red",
                               jitter_w = 0, jitter_h = 0.2,
                               legend.h = 25, legend.w = 0.5,
-                              show.mean = TRUE, mean.abs = TRUE, mean.round = 4) {
+                              show.mean = TRUE, mean.abs = TRUE, mean.round = 4,
+                              show.legend = TRUE,
+                              ord = NULL) {
 
   shaps = shaps[complete.cases(shaps), ]
 
-  p = ggplot(shaps, aes(x = Shap, y = Var, colour = relTruth))  +
-    geom_vline(xintercept = 0, colour = "darkgray") +
-    geom_point() + geom_jitter(width = jitter_w, height = jitter_h) +
-    scale_color_gradient(low = low, high = high,
-                         n.breaks = 2L,
-                         labels = c("Low", "High"),
-                         guide = guide_colorbar(barwidth = legend.w, barheight = legend.h,
-                                                title.position = "right",
-                                                title = "Feature Value",
-                                                title.theme = element_text(angle = 90,
-                                                                           hjust = 0.5))) +
-    theme_minimal() +
-    theme(axis.title.y = element_blank(), panel.grid = element_blank(),
-          axis.line.x = element_line(), axis.text.y = element_text(size = 12),
-          legend.text = element_text(size = 11),
-          axis.text.x = element_text(size = 10)) +
-    labs(x = "SHAP Value")
+  if (is.null(ord)) {
+    ord = aggregate(Shap ~ Var, data = shaps, function(x) mean(abs(x)))
+    shaps$Var = factor(shaps$Var, levels = as.character(ord[order(ord$Shap) ,1]))
+  } else {
+    shaps$Var = factor(shaps$Var, levels = ord)
+  }
+
+  shaps[shaps$id == "NA", c(1, 3:5)] = NA
+
+
+  p = ggplot2::ggplot(shaps, ggplot2::aes(x = Shap, y = Var, colour = relTruth))  +
+    ggplot2::geom_vline(xintercept = 0, colour = "darkgray") +
+    ggplot2::geom_point() + ggplot2::geom_jitter(width = jitter_w, height = jitter_h) +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(axis.title.y = ggplot2::element_blank(),
+                   panel.grid = ggplot2::element_blank(),
+          axis.line.x = ggplot2::element_line(),
+          axis.text.y = ggplot2::element_text(size = 12),
+          legend.text = ggplot2::element_text(size = 11),
+          axis.text.x = ggplot2::element_text(size = 10)) +
+    ggplot2::labs(x = "SHAP Value")
+
+  if (show.legend) {
+    p = p + ggplot2::scale_color_gradient(low = low, high = high,
+                                 n.breaks = 2L,
+                                 labels = c("Low", "High"),
+                                 guide = ggplot2::guide_colorbar(barwidth = legend.w, barheight = legend.h,
+                                                        title.position = "right",
+                                                        title = "Feature Value",
+                                                        title.theme = ggplot2::element_text(angle = 90,
+                                                                                   hjust = 0.5)))
+  } else {
+    p = p + ggplot2::scale_color_gradient(low = low, high = high) +
+      ggplot2::theme(legend.position = "none")
+  }
+
+
 
   if (show.mean) {
     if (mean.abs) {
@@ -100,9 +129,9 @@ plot_shap_summary <- function(shaps, low = "blue", high = "red",
       shapmean = aggregate(Shap ~ Var, data = shaps, mean)[,2]
     }
 
-    p = p + geom_text(aes(x = x, y = y, colour = NULL),
+    p = p + ggplot2::geom_text(aes(x = x, y = y, colour = NULL),
                       data = data.frame(x = max(shaps$Shap) + 0.5, y = 1:length(shapmean)),
-                      label = round(shapmean, mean.round))
+                      label = format(round(shapmean, mean.round), nsmall = mean.round))
   }
 
   p
@@ -132,35 +161,78 @@ plot_shap_importance <- function(shaps, relative = TRUE, bar_width = 0.4,
     xlim = c(0, (max(agg$Shap) - (max(agg$Shap) %% 10)) + 10)
   }
 
-  agg = agg[order(agg$Shap, decreasing = FALSE), ]
-  agg$Var = factor(agg$Var, levels = agg$Var)
 
-  p = ggplot(agg,
-             aes(x = Shap, y = Var, fill = Mean)) +
-    geom_bar(stat = "identity", width = bar_width) +
-    scale_fill_gradient(low = low, high = high, n.breaks = 3,
+  p = ggplot2::ggplot(agg,
+                      ggplot2::aes(x = Shap, y = Var, fill = Mean)) +
+    ggplot2::geom_bar(stat = "identity", width = bar_width) +
+    ggplot2::scale_fill_gradient(low = low, high = high, n.breaks = 3,
                         limits = legend_limit,
-                        guide = guide_colorbar(barwidth = legend.w, barheight = legend.h,
+                        guide = ggplot2::guide_colorbar(barwidth = legend.w, barheight = legend.h,
                                                title.position = "right",
                                                title = "Mean SHAP Value",
-                                               title.theme = element_text(angle = 90,
+                                               title.theme = ggplot2::element_text(angle = 90,
                                                                           hjust = 0.5))) +
-    theme_minimal() +
-    theme(axis.title.y = element_blank(), panel.grid = element_blank(),
-          axis.line.y = element_line(), axis.text.y = element_text(size = 12),
-          legend.text = element_text(size = 11),
-          axis.text.x = element_text(size = 10),
-          axis.ticks = element_line(),
+    ggplot2::theme_minimal() +
+    ggplot2::theme(axis.title.y = ggplot2::element_blank(),
+                   panel.grid = ggplot2::element_blank(),
+                   axis.line.y = ggplot2::element_line(),
+                   axis.text.y = ggplot2::element_text(size = 12),
+          legend.text = ggplot2::element_text(size = 11),
+          axis.text.x = ggplot2::element_text(size = 10),
+          axis.ticks = ggplot2::element_line(),
           legend.position = legend.position) +
-    xlim(xlim) +
-    labs(x = "Feature Contribution (%)")
+    ggplot2::xlim(xlim) +
+    ggplot2::labs(x = "Feature Contribution (%)")
 
   if (show.mean) {
-    p = p + geom_text(label = round(agg$Mean, mean.round),
+    p = p + ggplot2::geom_text(label = format(round(agg$Mean, mean.round), nsmall = mean.round),
                       hjust = -0.4)
   }
 
   p
+
+}
+
+plot_shap_sidebyside_importance <- function(shaps, bar_width = 0.4, ylim = NULL,
+                                            legend.position = "top") {
+
+  agg <- data.frame()
+  for (i in seq_along(shaps)) {
+    dat <- shaps[[i]][complete.cases(shaps[[i]]), ]
+    dat[dat == "NA", c(1, 3:5)] <- NA
+    shapi <- cbind(Data = names(shaps)[[i]],
+                   aggregate(Shap ~ Var, data = dat, function(x) mean(abs(x)),
+                             na.action = na.pass))
+    shapi$Shap <- (shapi$Shap/sum(shapi$Shap, na.rm = TRUE)) * 100
+    agg <- rbind(agg, shapi)
+  }
+
+  if (is.null(ylim)) {
+    ylim = c(0, (max(agg$Shap, na.rm = TRUE) - (max(agg$Shap, na.rm = TRUE) %% 10)) + 10)
+  }
+
+  ord = order(agg[agg$Data == names(shaps)[[1]], "Shap"], decreasing = TRUE)
+  agg$Var = factor(agg$Var, levels = levels(agg$Var)[ord])
+
+  ggplot2::ggplot(agg,
+                  ggplot2::aes(y = Shap, x = Var, fill = Data, group = Data)) +
+    ggplot2::geom_bar(stat = "identity", width = bar_width, position = "dodge",
+             color = "black") +
+    ggplot2::geom_text(label = round(agg$Shap), position = ggplot2::position_dodge(bar_width),
+              vjust = -1) +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(axis.title.x = ggplot2::element_blank(),
+          panel.grid = ggplot2::element_blank(),
+          axis.line.y = ggplot2::element_line(),
+          axis.text.x = ggplot2::element_text(size = 12),
+          legend.text = ggplot2::element_text(size = 11),
+          axis.text.y = ggplot2::element_text(size = 10),
+          axis.ticks = ggplot2::element_line(),
+          legend.title = ggplot2::element_blank(),
+          legend.position = legend.position,
+          legend.box = "horizontal") +
+    ggplot2::ylim(ylim) +
+    ggplot2::labs(y = "Feature Contribution (%)")
 
 }
 
@@ -181,45 +253,28 @@ plot_shap_interaction <- function(shaps, shap_feature, intX_feature,
   }
   data = data.frame(SHAPS = int_shaps, XTruth = Xtruth, ZTruth = Ztruth)
 
-  p = ggplot(data, aes(x = XTruth, y = SHAPS, color = ZTruth)) +
-    scale_color_gradient(low = low, high = high, n.breaks = 3,
-                         guide = guide_colorbar(title = intZ_feature,
+  p = ggplot2::ggplot(data, ggplot2::aes(x = XTruth, y = SHAPS, color = ZTruth)) +
+    ggplot2::scale_color_gradient(low = low, high = high, n.breaks = 3,
+                         guide = ggplot2::guide_colorbar(title = intZ_feature,
                                                 title.position = "right",
-                                                title.theme = element_text(angle = 90,
+                                                title.theme = ggplot2::element_text(angle = 90,
                                                                            hjust = 0.5))) +
-    theme_minimal() +
-    theme(panel.grid = element_blank(),
-          axis.line.y = element_line(), axis.line.x = element_line(),
-          axis.ticks = element_line()) +
-    labs(x = intX_feature,
+    ggplot2::theme_minimal() +
+    ggplot2::theme(panel.grid = ggplot2::element_blank(),
+          axis.line.y = ggplot2::element_line(),
+          axis.line.x = ggplot2::element_line(),
+          axis.ticks = ggplot2::element_line()) +
+    ggplot2::labs(x = intX_feature,
          y = paste("SHAP Interaction Values\n", shap_feature, "X", intX_feature))
 
   if (type == "p" | type == "b") {
-    p = p + geom_point()
+    p = p + ggplot2::geom_point()
   }
   if (type == "s" | type == "b") {
-    p = p + geom_smooth(se = se, color = smooth_color, fill = smooth_fill,
+    p = p + ggplot2::geom_smooth(se = se, color = smooth_color, fill = smooth_fill,
                         lwd = smooth_lwd, level = smooth_level)
   }
 
   p
 
 }
-
-# library(mlr3proba)
-# library(mlr3pipelines)
-#
-# set.seed(1)
-# task = po("encode", method = "treatment")$train(list(tsk("lung")))$output
-# lrn = lrn("surv.xgboost")
-# lrn$train(task)
-#
-# shaps <- get_shaps(lrn$model, task)
-# plot_shap_feature(shaps, "age", type = "s", se = TRUE, xlab = "Height")
-# plot_shap_feature(shaps, "inst", type = "b", se = TRUE, xlab = "Weight")
-# plot_shap_feature(shaps, "sex", type = "p", se = TRUE, xlab = "Treatment")
-# plot_shap_summary(shaps)
-# plot_shap_importance(shaps, legend.position = "right")
-#
-# shap_ints <- get_interaction_shaps(lrn$model, task)
-# plot_shap_interaction(shap_ints, "ph.ecog", "pat.karno", "sex")
