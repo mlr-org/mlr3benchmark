048f0435e894469a97a044835fb14e8a626e26ca
john <bblodfon@gmail.com>
2023-08-26 14:52:43 +0200

    add BenchmarkScore class + tests

---
+++ R/BenchmarkScore.R
@@ -0,0 +1,277 @@
+#' @title Benchmark Score Result Object
+#'
+#' @description An R6 class for the non-aggregated benchmark results.
+#' @details This class is used to easily carry out and guide analysis of models
+#' using the benchmarking scores on the different resamplings. The class can
+#' either be constructed using \CRANpkg{mlr3} objects, for example the result
+#' of [mlr3::BenchmarkResult]`$score` or via [as_benchmark_score], or by passing
+#' in a custom dataset of results. Custom datasets must include at the very least,
+#' a factor column for learner ids, a factor column for task ids, a numeric
+#' column for the resampling iterations (e.g. 1,2,3,...) and numeric columns
+#' for one or more measures.
+#'
+#' Supported for both multiple dependent and independent datasets.
+#'
+#' @references
+#' `r format_bib("benavoli_2017")`
+#'
+#' @examples
+#' # Not restricted to mlr3 objects
+#' df = data.frame(
+#'   tasks     = factor(rep(c("A", "B"), each = 25)),
+#'   learners  = factor(rep(paste0("L", 1:5), each = 5)),
+#'   iters     = 1:5,
+#'   RMSE      = runif(50),
+#'   MAE       = runif(50)
+#' )
+#' BenchmarkScore$new(df, task_id = "tasks", learner_id = "learners", iteration = "iters")
+#' # equivalently
+#' as_benchmark_score(df, task_id = "tasks", learner_id = "learners", iteration = "iters")
+#'
+#' if (requireNamespaces(c("mlr3", "rpart"))) {
+#'   library(mlr3)
+#'   tasks = tsks(c("boston_housing", "mtcars"))
+#'   learners = lrns(c("regr.featureless", "regr.rpart"))
+#'   bm = benchmark(benchmark_grid(tasks, learners, rsmp("cv", folds = 3)))
+#'
+#'   # coercion from mlr3 BenchmarkResult object
+#'   as_benchmark_score(bm)
+#' }
+#' @export
+BenchmarkScore = R6Class("BenchmarkScore",
+  public = list(
+    #' @description
+    #' Creates a new instance of this [R6][R6::R6Class] class.
+    #' @param dt `(matrix(1))`\cr
+    #' A `matrix` like object coercable to [data.table::data.table][data.table],
+    #' should include column names "task_id", "learner_id", "iteration", and at
+    #' least one measure (numeric).
+    #' @param task_id (`factor(1)`) \cr
+    #' String specifying name of task id column.
+    #' @param learner_id (`factor(1)`)\cr
+    #' String specifying name of learner id column.
+    #' @param iteration (`character(1)`)\cr
+    #' String specifying name of iteration id column.
+    #' @param independent `(logical(1))` \cr
+    #' Are tasks independent of one another? Affects which tests can be used for analysis.
+    #' @param strip_prefix (`logical(1)`) \cr
+    #' If `TRUE` (default) then mlr prefixes, e.g. `regr.`, `classif.`, are automatically
+    #' stripped from the `learner_id` and other columns (i.e. representing measure ids).
+    #' @param ... `ANY` \cr
+    #' Additional arguments, currently unused.
+    initialize = function(dt, task_id = "task_id", learner_id = "learner_id",
+      iteration = "iteration", independent = TRUE, strip_prefix = TRUE, ...) {
+
+      if (!is.data.table(dt)) {
+        dt = as.data.table(dt)
+      }
+
+      private$.independent = assert_flag(independent)
+      assert_flag(strip_prefix)
+      assert_subset(c(task_id, learner_id, iteration), colnames(dt))
+      assert_factor(unlist(subset(dt, select = task_id)))
+      assert_factor(unlist(subset(dt, select = learner_id)))
+      assert_numeric(unlist(subset(dt, select = iteration)))
+
+      private$.col_roles = list(
+        task_id = task_id,
+        learner_id = learner_id,
+        iteration = iteration
+      )
+      # exclude column names that are part of `BenchmarkResult$score()`
+      measure_ids = setdiff(colnames(dt), c(task_id, learner_id, iteration,
+        "uhash", "nr", "task", "learner", "resampling", "resampling_id",
+        "prediction"))
+
+      if (length(measure_ids) == 0L) {
+        stop("At least one measure must be included in `dt`.")
+      }
+
+      dt = dt[, c(task_id, learner_id, iteration, measure_ids), with = FALSE]
+      # confirm all measures numeric
+      assert_data_frame(dt[, measure_ids, with = FALSE], types = "numeric")
+      # confirm at least 2 iterations
+      niters = length(unique(dt[[iteration]]))
+      if (niters < 2) {
+        stop("Less than two resamplings - better use BenchmarkAggr()")
+      }
+
+      # to check with Bernd: can do both!
+      # if (!independent) {
+      #   warning("Currently only methods for independent datasets are supported.")
+      # }
+
+      if (anyDuplicated(dt, by = c(task_id, learner_id, iteration))) {
+        stop("Multiple results for a learner-task-iteration combination detected. There should be exactly one row for each learner-task-iteration combination.") # nolint
+      }
+
+      # check 'full' benchmark results? Every task with every learner and every resampling!!!
+      # SHOULD WE FORCE THIS?
+      # stop("Not complete benchmark design")
+
+      if (strip_prefix) {
+        if (isNamespaceLoaded("mlr3")) {
+          types = mlr3::mlr_reflections$task_types$type
+        } else {
+          types = c("regr", "classif", "surv", "dens", "clust")
+        }
+        pattern = sprintf("^(%s)\\.", paste0(types, collapse = "|"))
+        levels(dt[[learner_id]]) = gsub(pattern, "", levels(dt[[learner_id]]))
+        colnames(dt) = gsub(pattern, "", colnames(dt))
+      }
+
+      private$.dt = dt
+
+      # At least 2 tasks
+      if (self$ntasks < 2) {
+        warning("Currently only supported for multiple tasks.")
+      }
+
+      # Restrict number of learners? (ask Bernd)
+
+      invisible(self)
+    },
+
+    #' @description
+    #' Prints the internal data via [data.table::print.data.table].
+    #' @param ... `ANY` \cr Passed to [data.table::print.data.table].
+    print = function(...) {
+      catf("<BenchmarkScore> of %i %s with %i %s, %i %s, %i resampling iterations and %i %s",
+        self$nrow, ifelse(self$nrow == 1, "row", "rows"),
+        self$ntasks, ifelse(self$ntasks == 1, "task", "tasks"),
+        self$nlrns, ifelse(self$nlrns == 1, "learner", "learners"),
+        self$niters,
+        self$nmeas, ifelse(self$nmeas == 1, "measure", "measures"))
+      print(private$.dt, ...)
+    },
+
+    #' @description
+    #' Prints the internal data via [data.table::print.data.table].
+    #' @param ... `ANY` \cr Passed to [data.table::print.data.table].
+    summary = function(...) {
+      self$print(...)
+    },
+
+    #' @description Subsets the data by given tasks, learners and iterations.
+    #' Returns data as [data.table::data.table].
+    #' @param tasks (`character()`) \cr
+    #' Task(s) to subset the data by.
+    #' @param learners (`character()`) \cr
+    #' Learner(s) to subset the data by.
+    #' @param iterations (`numeric()`) \cr
+    #' Resampling iteration(s) to subset the data by.
+    subset = function(tasks = NULL, learners = NULL, iterations = NULL) {
+      dt = private$.dt
+
+      if (!is.null(tasks)) {
+        rows = dt[[self$col_roles$task_id]] %in% tasks
+        dt = dt[rows]
+      }
+      if (!is.null(learners)) {
+        rows = dt[[self$col_roles$learner_id]] %in% learners
+        dt = dt[rows]
+      }
+      if (!is.null(iterations)) {
+        rows = dt[[self$col_roles$iteration]] %in% iterations
+        dt = dt[rows]
+      }
+
+      dt
+    }
+  ),
+
+  active = list(
+    #' @field data ([data.table::data.table]) \cr Resampled scores data.
+    data = function() private$.dt,
+    #' @field learners `(character())` \cr Unique learner names.
+    learners = function() levels(private$.dt[[self$col_roles$learner_id]]),
+    #' @field tasks `(character())` \cr Unique task names.
+    tasks = function() levels(private$.dt[[self$col_roles$task_id]]),
+    #' @field iterations `(numeric())` \cr
+    iterations = function() unique(private$.dt[[self$col_roles$iteration]]),
+    #' @field measures `(character())` \cr Unique measure names.
+    measures = function() setdiff(colnames(private$.dt), unlist(self$col_roles)),
+    #' @field nlrns `(integer())` \cr Number of learners.
+    nlrns = function() length(self$learners),
+    #' @field ntasks `(integer())` \cr Number of tasks.
+    ntasks = function() length(self$tasks),
+    #' @field niters `(integer()` \cr Number of resampling iterations.
+    niters = function() length(self$iterations),
+    #' @field nmeas `(integer())` \cr Number of measures.
+    nmeas = function() length(self$measures),
+    #' @field nrow `(integer())` \cr Number of rows.
+    nrow = function() nrow(self$data),
+    #' @field col_roles (`character()`) \cr
+    #' Column roles, currently cannot be changed after construction.
+    col_roles = function() {
+      private$.col_roles
+    }
+  ),
+
+  private = list(
+    .col_roles = character(0),
+    .dt = data.table(),
+    .independent = logical(0)
+  )
+)
+
+#' @title Coercions to BenchmarkScore
+#'
+#' @description Coercion methods to [BenchmarkScore].
+#' For [mlr3::BenchmarkResult] this is a simple wrapper around the
+#' [BenchmarkScore] constructor called with [mlr3::BenchmarkResult]`$score()`.
+#'
+#' @param obj ([mlr3::BenchmarkResult]|`matrix(1)`) \cr Passed to [BenchmarkScore]`$new()`.
+#' @param task_id,learner_id,iteration,independent,strip_prefix See [BenchmarkScore]`$initialize()`.
+#' @param ... `ANY` \cr Passed to [mlr3::BenchmarkResult]`$score()`.
+#' @examples
+#' # From a data.frame object
+#' df = data.frame(
+#'   tasks     = factor(rep(c("A", "B"), each = 25)),
+#'   learners  = factor(rep(paste0("L", 1:5), each = 5)),
+#'   iters     = 1:5,
+#'   RMSE      = runif(50),
+#'   MAE       = runif(50)
+#' )
+#' bms = as_benchmark_score(df, task_id = "tasks", learner_id = "learners", iteration = "iters")
+#'
+#' if (requireNamespaces(c("mlr3", "rpart"))) {
+#'   library(mlr3)
+#'   tasks = tsks(c("boston_housing", "mtcars"))
+#'   learners = lrns(c("regr.featureless", "regr.rpart"))
+#'   bm = benchmark(benchmark_grid(tasks, learners, rsmp("cv", folds = 3)))
+#'
+#'   # default measure
+#'   as_benchmark_score(bm)
+#'
+#'   # change measure
+#'   as_benchmark_score(bm, measures = msrs(c("regr.rmse", "regr.mae")))
+#' }
+#'
+#' @export
+as_benchmark_score = function(obj, task_id = "task_id", learner_id = "learner_id",
+  iteration = "iteration", independent = TRUE, strip_prefix = TRUE, ...) {
+  UseMethod("as_benchmark_score", obj)
+}
+
+#' @export
+as_benchmark_score.default = function(obj, task_id = "task_id",
+  learner_id = "learner_id", iteration = "iteration", independent = TRUE,
+  strip_prefix = TRUE, ...) {
+  BenchmarkScore$new(obj, task_id = task_id, learner_id = learner_id,
+    iteration = iteration, independent = independent, strip_prefix = strip_prefix)
+}
+
+#' @export
+as_benchmark_score.BenchmarkResult = function(obj, task_id = "task_id",
+  learner_id = "learner_id", iteration = "iteration", independent = TRUE,
+  strip_prefix = TRUE, measures = NULL, ...) {
+  requireNamespaces("mlr3")
+  measures = mlr3::as_measures(measures, task_type = obj$task_type)
+  tab = obj$score(measures = measures)
+  cols = c("task_id", "learner_id", "iteration", map_chr(measures, "id"))
+  tab$task_id = factor(tab$task_id, levels = unique(tab$task_id))
+  tab$learner_id = factor(tab$learner_id, levels = unique(tab$learner_id))
+  BenchmarkScore$new(tab[, cols, with = FALSE], independent = independent,
+    strip_prefix = strip_prefix)
+}
---
+++ tests/testthat/test_BenchmarkScore.R
@@ -0,0 +1,157 @@
+test_that("construction", {
+  df = data.frame(
+    task_id    = factor(rep(c("A", "B"), each = 25)),
+    learner_id = factor(rep(paste0("L", 1:5), each = 5)),
+    iteration  = 1:5,
+    RMSE       = runif(50)
+  )
+  expect_silent(BenchmarkScore$new(df))
+
+  # check that at least one measure is included
+  df2 = subset(df, select = c("task_id", "learner_id", "iteration"))
+  expect_error(BenchmarkScore$new(df2), "At least one measure")
+
+  # no warning here, delete after cross checking with Bernd
+  # expect_warning(BenchmarkScore$new(df, independent = FALSE), "independent datasets")
+
+  df = data.frame(
+    tasks     = rep(c("A", "B"), each = 25),
+    learners  = rep(paste0("regr.", 1:5), each = 5),
+    iters     = 1:5,
+    regr.mae  = runif(50),
+    regr.mse  = runif(50),
+    stringsAsFactors = TRUE
+  )
+
+  # check different column names
+  bms = BenchmarkScore$new(df, task_id = "tasks", learner_id = "learners",
+    iteration = "iters")
+  expect_equal(bms$learners, as.character(1:5))
+  expect_equal(bms$measures, c("mae", "mse"))
+
+  # check striping of mlr3 prefixes
+  bms = BenchmarkScore$new(df, task_id = "tasks", learner_id = "learners",
+    iteration = "iters", strip_prefix = FALSE)
+  expect_equal(bms$learners, paste0("regr.", 1:5))
+  expect_equal(bms$measures, c("regr.mae", "regr.mse"))
+
+  # At least 2 tasks should be provided
+  df = data.frame(
+    task_id    = rep("A", each = 25),
+    learner_id = rep(paste0("L", 1:5), each = 5),
+    iteration  = 1:5,
+    RMSE       = runif(25),
+    stringsAsFactors = TRUE
+  )
+  expect_warning(BenchmarkScore$new(df), "multiple tasks")
+
+  # Same task-learner-iteration combination is not allowed
+  df = data.frame(
+    task_id    = c("A", "A", "B", "B", "B"),
+    learner_id = rep("L1", each = 5),
+    iteration  = c(1,2,1,2,2),
+    RMSE       = runif(5),
+    stringsAsFactors = TRUE
+  )
+  expect_error(BenchmarkScore$new(df), "combination")
+
+  # Should have at least 2 iterations
+  df = data.frame(
+    task_id    = rep(c("A", "B"), each = 2),
+    learner_id = rep(c("L1", "L2"), times = 2),
+    iteration  = 1,
+    RMSE       = runif(4),
+    stringsAsFactors = TRUE
+  )
+  expect_error(BenchmarkScore$new(df), "Less than two resamplings")
+})
+
+test_that("public methods", {
+  set.seed(1)
+  rmse = round(runif(18, 1, 9))
+  mse = rmse^2
+  df = data.frame(
+    tasks     = rep(c("A", "B"), each = 9),
+    learners  = rep(paste0("L", 1:3), each = 3),
+    iters     = 1:3,
+    RMSE      = rmse,
+    MSE       = mse,
+    stringsAsFactors = TRUE
+  )
+  bms = BenchmarkScore$new(df, task_id = "tasks", learner_id = "learners",
+    iteration = 'iters')
+
+  # print()
+  expect_output(print(bms),
+    "18 rows with 2 tasks, 3 learners, 3 resampling iterations and 2 measures")
+  # summary()
+  expect_output(bms$summary(),
+    "18 rows with 2 tasks, 3 learners, 3 resampling iterations and 2 measures")
+  # subset()
+  empty_dt = data.table(tasks = factor(), learners = factor(), iters = numeric(),
+    RMSE = numeric(), MSE = numeric())
+  expect_equal(bms$subset(), bms$data) # no subsetting
+  expect_equal(bms$subset(tasks = c("A", "B")), bms$data)
+  expect_equal(bms$subset(tasks = "C42"), empty_dt)
+  expect_equal(bms$subset(learners = "L42"), empty_dt)
+  expect_equal(bms$subset(iterations = 0), empty_dt)
+  expect_equal(
+    bms$subset(tasks = "A", learners = "L1", iterations = 1:2),
+    data.table(tasks = factor("A"), learners = factor("L1"), iters = 1:2,
+               RMSE = c(3,4), MSE = c(9,16))
+  )
+  expect_equal(
+    bms$subset(learners = "L3", iterations = 2),
+    data.table(tasks = factor(c("A", "B")), learners = factor(c("L3", "L3")),
+               iters = c(2, 2), RMSE = c(6, 7), MSE = c(36, 49))
+  )
+  expect_equal(
+    bms$subset(tasks = c("B", "C42"), learners = c("L1", "L42", "L3"), iterations = 3),
+    data.table(tasks = factor("B"), learners = factor(c("L1", "L3")), iters = c(3,3),
+               RMSE = c(2, 9), MSE = c(4, 81))
+  )
+})
+
+test_that("active bindings", {
+  df = data.frame(
+    tasks     = factor(rep(c("A", "B"), each = 25)),
+    learners  = factor(rep(paste0("L", 1:5), each = 5)),
+    iteration = 1:5,
+    RMSE      = runif(50),
+    MSE       = runif(50)
+  )
+  bms = BenchmarkScore$new(df, task_id = "tasks", learner_id = "learners")
+
+  expect_equal(class(bms$data)[1], "data.table")
+  expect_equal(colnames(bms$data), colnames(df))
+  expect_equal(bms$learners, paste0("L", 1:5))
+  expect_equal(bms$tasks, c("A", "B"))
+  expect_equal(bms$iterations, 1:5)
+  expect_equal(bms$measures, c("RMSE", "MSE"))
+  expect_equal(bms$col_roles$task_id, "tasks")
+  expect_equal(bms$col_roles$learner_id, "learners")
+  expect_equal(bms$col_roles$iteration, "iteration")
+  expect_equal(bms$nlrns, 5)
+  expect_equal(bms$ntasks, 2)
+  expect_equal(bms$niters, 5)
+  expect_equal(bms$nmeas, 2)
+  expect_equal(bms$nrow, 50)
+})
+
+test_that("mlr3 coercions", {
+  skip_if_not_installed("mlr3")
+  skip_if_not_installed("rpart")
+
+  library(mlr3)
+  tasks = tsks(c("boston_housing", "mtcars"))
+  learners = lrns(c("regr.featureless", "regr.rpart"))
+  bm = benchmark(benchmark_grid(tasks, learners, rsmp("cv", folds = 2)))
+  expect_equal(class(as_benchmark_score(bm))[1], "BenchmarkScore")
+  expect_equal(class(as_benchmark_score(bm, measures = msr("regr.mae")))[1], "BenchmarkScore")
+  tab = bm$score(measures = msrs(c("regr.rmse", "regr.mae")))
+  tab$task_id = factor(tab$task_id)
+  tab$learner_id = factor(tab$learner_id)
+  bms = BenchmarkScore$new(tab)
+  expect_equal(class(bms)[1], "BenchmarkScore")
+  expect_equal(bms$measures, c("rmse", "mae"))
+})
