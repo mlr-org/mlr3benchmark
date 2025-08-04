f4a02edfd228d2f30a8f9017babdb90ce6d1aa06
Lukas Burk <lukas@quantenbrot.de>
2025-08-04 14:46:18 +0200

    Use is_ggplot in favor of deprecated is.ggplot()

--- tests/testthat/test_autoplot_BenchmarkAggr.R
+++ tests/testthat/test_autoplot_BenchmarkAggr.R
@@ -5,12 +5,12 @@ test_that("autoplot.BenchmarkAggr", {
                   RMSE = runif(100, 0, 1:100))
   ba = BenchmarkAggr$new(df)
 
-  expect_true(is.ggplot(autoplot(ba, type = "mean")))
-  expect_true(is.ggplot(autoplot(ba, type = "box")))
+  expect_true(is_ggplot(autoplot(ba, type = "mean")))
+  expect_true(is_ggplot(autoplot(ba, type = "box")))
 
   skip_if_not_installed("PMCMRplus")
-  expect_true(is.ggplot(autoplot(ba, type = "fn")))
-  expect_true(is.ggplot(autoplot(ba, type = "cd")))
+  expect_true(is_ggplot(autoplot(ba, type = "fn")))
+  expect_true(is_ggplot(autoplot(ba, type = "cd")))
 })
 
 test_that("autoplot.BenchmarkAggr cd", {
@@ -22,13 +22,13 @@ test_that("autoplot.BenchmarkAggr cd", {
                   RMSE = runif(5))
   ba = BenchmarkAggr$new(df)
 
-  expect_error(is.ggplot(autoplot(ba, type = "fn")))
-  expect_true(is.ggplot(expect_warning(autoplot(ba, type = "fn", friedman_global = FALSE))))
-  expect_silent(is.ggplot(autoplot(ba, type = "fn", p.value = 1, test = "bd")))
-  expect_error(is.ggplot(autoplot(ba, type = "cd")))
-  expect_true(is.ggplot(expect_warning(autoplot(ba, type = "cd", friedman_global = FALSE))))
-  expect_silent(is.ggplot(autoplot(ba, type = "cd", p.value = 1, test = "bd")))
-  expect_silent(is.ggplot(autoplot(ba, type = "cd", p.value = 1, test = "bd", style = 2)))
+  expect_error(is_ggplot(autoplot(ba, type = "fn")))
+  expect_true(is_ggplot(expect_warning(autoplot(ba, type = "fn", friedman_global = FALSE))))
+  expect_silent(is_ggplot(autoplot(ba, type = "fn", p.value = 1, test = "bd")))
+  expect_error(is_ggplot(autoplot(ba, type = "cd")))
+  expect_true(is_ggplot(expect_warning(autoplot(ba, type = "cd", friedman_global = FALSE))))
+  expect_silent(is_ggplot(autoplot(ba, type = "cd", p.value = 1, test = "bd")))
+  expect_silent(is_ggplot(autoplot(ba, type = "cd", p.value = 1, test = "bd", style = 2)))
 })
 
 test_that("autoplot with BenchmarkAggr from mlr3::benchmark()", {
@@ -47,9 +47,9 @@ test_that("autoplot with BenchmarkAggr from mlr3::benchmark()", {
   )
   bm = benchmark(benchmark_grid(task, learns, rsmp("cv", folds = 3)))
   ba = as_benchmark_aggr(bm)
-  expect_warning(expect_true(ggplot2::is.ggplot(autoplot(ba,
+  expect_warning(expect_true(ggplot2::is_ggplot(autoplot(ba,
     type = "cd",
     friedman_global = FALSE
   ))))
-  expect_true(ggplot2::is.ggplot(autoplot(ba, type = "cd", p.value = 0.2)))
+  expect_true(ggplot2::is_ggplot(autoplot(ba, type = "cd", p.value = 0.2)))
 })
